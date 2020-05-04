83b8097af1d0663d2f5f7352c1b0d95ab57252a5
ynigoreyes <ynigoreyes@gmail.com>
2020-05-04 14:42:12 -0500

    adding program, stmt_list

--- Parser.py
+++ Parser.py
@@ -5,13 +5,33 @@ class Parser:
     def __init__(self, a: List):
         self.s = Scanner(a)
         self.tokens = self.s.scan()
-        self.list = self.s.list()   
+        self.tokenValues = self.s.list()
+        self.currentTokenPosition = 0
 
 
     def parse(self):
         
         print('token list: {0}'.format(self.tokens[:]))
-        print('<Program>\n\t<stmt_list>\n\t\t<stmt>')
-        #put code here...
-        print('\t\t</stmt>\n\t\t<stmt_list>\n\t\t</stmt_list>\n\t</stmt_list>\n</Program>')
+        self.__program__(0)
     
+    def __program__(self, level: int):
+        self.__print_with_indent__('<program>', level)
+        self.__stmt_list__(level + 1)
+        self.__print_with_indent__('</program>', level)
+
+    def __stmt_list__(self, level: int):
+        self.__print_with_indent__('<stmt_list>', level)
+		# If there is still something left to read
+        if self.currentTokenPosition != len(self.tokenValues) - 1:
+            self.__stmt__(level + 1)
+            self.__stmt_list__(level + 1)
+        self.__print_with_indent__('</stmt_list>', level)
+
+    def __stmt__(self, level: int):
+        self.__print_with_indent__('<stmt>', level)
+        # Just so that we don't go into stack overflow
+        self.currentTokenPosition += 1
+        self.__print_with_indent__('</stmt>', level)
+
+    def __print_with_indent__(self, value, level: int):
+        print(level * '\t' + value)
