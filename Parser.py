614b2634d46b2b7a0b89a9afd40a8e75cc5b53db
Eliascm17 <Elias.cmoreno17@gmail.com>
2020-05-04 14:41:57 -0500

    function declarations

--- Parser.py
+++ Parser.py
@@ -1,17 +1,52 @@
 from typing import List
 from Scanner import Scanner
+
 class Parser:
 
-    def __init__(self, a: List):
-        self.s = Scanner(a)
-        self.tokens = self.s.scan()
-        self.list = self.s.list()   
+	def __init__(self, a: List):
+		self.s = Scanner(a)
+		self.tokens = self.s.scan()
+		self.list = self.s.list()
 
+	def program(self):
+		pass
 
-    def parse(self):
-        
-        print('token list: {0}'.format(self.tokens[:]))
-        print('<Program>\n\t<stmt_list>\n\t\t<stmt>')
-        #put code here...
-        print('\t\t</stmt>\n\t\t<stmt_list>\n\t\t</stmt_list>\n\t</stmt_list>\n</Program>')
-    
+	def stmt_list(self):
+		pass
+
+	def stmt(self):
+		pass
+
+	def expr(self):
+		pass
+
+	def term(self):
+		#if factor then:
+        #	factor()
+        #   factor_tail()
+        #else:
+        #	returnpass
+		pass
+
+	def term_tail(self):
+		#if nothing:
+        #	return
+        #else:
+        #	mult_op()
+		#	term()
+		#	term_tail()
+		pass
+
+	def factor(self):
+		pass
+
+	def fact_tail(self):
+		pass
+
+	def add_op(self):
+		pass
+
+	def mult_op(self):
+		pass
+
+       
