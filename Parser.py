8897c24266c5f8eef0e09a5f4bc52b74c08692dc
ynigoreyes <ynigoreyes@gmail.com>
2020-05-04 15:12:34 -0500

    make everything public

--- Parser.py
+++ Parser.py
@@ -10,32 +10,32 @@ class Parser:
 	   
 	def parse(self): 
 		print('token list: {0}'.format(self.tokens[:]))
-		self.__program__(0)
+		self.program(0)
 	
-	def __program__(self, level: int):
-		self.__print_with_indent__('<program>', level)
-		self.__stmt_list__(level + 1)
-		self.__print_with_indent__('</program>', level)
+	def program(self, level: int):
+		self.print_with_indent('<program>', level)
+		self.stmt_list(level + 1)
+		self.print_with_indent('</program>', level)
 
-	def __stmt_list__(self, level: int):
-		self.__print_with_indent__('<stmt_list>', level)
+	def stmt_list(self, level: int):
+		self.print_with_indent('<stmt_list>', level)
 		# If there is still something left to read
 		if self.currentTokenPosition < len(self.tokenValues):
-			self.__stmt__(level + 1)
-			self.__stmt_list__(level + 1)
-		self.__print_with_indent__('</stmt_list>', level)
+			self.stmt(level + 1)
+			self.stmt_list(level + 1)
+		self.print_with_indent('</stmt_list>', level)
 
-	def __stmt__(self, level: int):
-		self.__print_with_indent__('<stmt>', level)
+	def stmt(self, level: int):
+		self.print_with_indent('<stmt>', level)
 		# If the next token is an id
-		if self.__match__('id'):
-			self.__print_with_indent__('<id>', level)
-			self.__print_with_indent__(self.tokenValues[self.currentTokenPosition], level + 1)
-			self.__print_with_indent__('</id>', level)
+		if self.match('id'):
+			self.print_with_indent('<id>', level)
+			self.print_with_indent(self.tokenValues[self.currentTokenPosition], level + 1)
+			self.print_with_indent('</id>', level)
 		
 		self.currentTokenPosition += 1
 
-		self.__print_with_indent__('</stmt>', level)
+		self.print_with_indent('</stmt>', level)
 
 	def expr(self):
 		pass
@@ -70,7 +70,7 @@ class Parser:
 		pass
 
 	# Returns true if the next value matches the value provided
-	def __match__(self, value):
+	def match(self, value):
 		# If we are about to look ahead into out of bounds for the array, error out
 		try:
 			return self.tokens[self.currentTokenPosition + 1] == value
@@ -78,5 +78,5 @@ class Parser:
 			return False
 
 
-	def __print_with_indent__(self, value, level: int):
+	def print_with_indent(self, value, level: int):
 		print(level * '\t' + value)
